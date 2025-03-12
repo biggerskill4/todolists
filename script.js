5b32354a8ea5e680d30b89df4c071d917884157b
Subhan Ahmed <subhanahmede860@gmail.com>
2025-03-12 19:06:55 +0500

    New Upadtes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const toDoListForm = document.querySelector('.tdlForm');
 const itemField = document.querySelector('#itemField');
-const itemBtn = document.querySelector('.tdlForm input[type="submit"]');
+const itemBtn = document.querySelector('.tdlForm button[type="submit"]');
 const itemLists = document.querySelector('.displayItems');
+
+
 
 toDoListForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let itemFieldText = itemField.value;
+    if (itemFieldText === "") alert('Type something');
 
-    
+    let tasks = JSON.parse(localStorage.getItem("toDoList")) || [];
+    tasks.push(itemFieldText);
+    localStorage.setItem("toDoList", JSON.stringify(tasks));
+
+   
     console.log(itemFieldText);
-
-
-    itemFieldText = '';
+    itemField.value = '';
 })
